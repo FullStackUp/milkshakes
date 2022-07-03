ea586788e16497653af85bdfcc0a2c313ae3c66a
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-07-03 12:23:08 +0200

    Add milkshake-details component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MilkshakeListComponent } from './milkshake-list/milkshake-list.component';
+import { MilkshakeDetailsComponent } from './milkshake-details/milkshake-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MilkshakeListComponent
+    MilkshakeListComponent,
+    MilkshakeDetailsComponent
   ],
   imports: [
     BrowserModule
